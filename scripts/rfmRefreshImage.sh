a7a5f5a4663b2a1f1bc12079dd533e75cb4e6963
guyuming76 <gu_yu_ming@hotmail.com>
2023-06-28 20:05:37 +0800

    enhance rfmRefreshImage.sh. when multiselect and open pictures in rfm, user can now browse pictures with keyboard left right arrows in IMV

--- scripts/rfmRefreshImage.sh
+++ scripts/rfmRefreshImage.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-imv_pid=$(ps -u|grep -e imv -e for_rfm |grep -v grep|awk '{print $2}')
+if [ $# -gt 1 ]
+then
+	#rfm传入多个文件，则全部通过管道传给imv，用户使用键盘左右键翻图片
+	echo "$@"|xargs imv
+else
+	#rfm传入单个文件，则找到现有 imv 进程，刷新显示图片
+	imv_pid=$(ps -u|grep -e imv -e for_rfm |grep -v grep|awk '{print $2}')
 
-if test -z "$imv_pid" 
-then  
-    imv $1 for_rfm 
-else   
-    imv-msg $imv_pid close
-    imv-msg $imv_pid open $1
-fi 
+	if test -z "$imv_pid" 
+	then  
+    	imv $1 for_rfm 
+	else   
+    	imv-msg $imv_pid close
+    	imv-msg $imv_pid open $1
+	fi 
 
-#[[ -z "$imv_pid" ]] && echo "empty" || echo $imv_pid
+	#[[ -z "$imv_pid" ]] && echo "empty" || echo $imv_pid
+
+fi
