guyuming76/rfm

commit 6ec023530c2c22b8297e251b66e298f0cd924e4d

deal with situation when there is space in file path. add a TODO about %s escape

rfm.c
@@ -2824,7 +2824,6 @@ static void exec_stdin_command (gchar * readlineResult)
 	    if (stdin_cmd_ending_space){
 	      // combine runCmd with selected files to get gchar** v
 	      // TODO: the following code share the same pattern as g_spawn_wrapper_for_selected_fileList_ , anyway to remove the duplicate code?
-	      // TODO: if file name contains space, wrap name with ''
 	      GtkTreeIter iter;
 	      GList *listElement;
 	      stdin_cmd_selection_list=get_view_selection_list(icon_or_tree_view,treeview,&treemodel);
@@ -2836,11 +2835,11 @@ static void exec_stdin_command (gchar * readlineResult)
 		  gtk_tree_model_get (GTK_TREE_MODEL(store), &iter, COL_ATTR, &stdin_cmd_selection_fileAttributes, -1);
 
 		  //if there is %s in msg, replace it with selected filename one by one, otherwise, append filenames to the end.
-		  if (strstr(readlineResultString->str, "%s") == NULL) {
-		    g_string_append(readlineResultString, " ");
-		    g_string_append(readlineResultString, stdin_cmd_selection_fileAttributes->path);
-		  }else
-		    g_string_replace(readlineResultString, "%s",stdin_cmd_selection_fileAttributes->path, 1);
+		  //TODO: what if userinput need %s literally? how to escape?
+		  if (strstr(readlineResultString->str, "%s") == NULL) g_string_append(readlineResultString, "%s");
+		  //if file path contains space, wrap path inside ''
+                  if (strstr(stdin_cmd_selection_fileAttributes->path," ") != NULL) g_string_replace(readlineResultString, "%s", "'%s'", 1);
+		  g_string_replace(readlineResultString, "%s",stdin_cmd_selection_fileAttributes->path, 1);
 	      
 		  listElement=g_list_next(listElement);
 		}

commit 719e3eed54aaa5d48e234a3e3e809f66113d4724

fix bug caused by commit 6ec023530c2c22b8297e251b66e298f0cd924e4d. with more than one file in selection and append to the end, there should be space between file names

rfm.c
@@ -2837,7 +2837,7 @@ static void exec_stdin_command (gchar * readlineResult)
 
 		  //if there is %s in msg, replace it with selected filename one by one, otherwise, append filenames to the end.
 		  //TODO: what if userinput need %s literally? how to escape?
-		  if (strstr(readlineResultString->str, "%s") == NULL) g_string_append(readlineResultString, "%s");
+		  if (strstr(readlineResultString->str, "%s") == NULL) g_string_append(readlineResultString, "%s ");
 		  //if file path contains space, wrap path inside ''
                   if (strstr(stdin_cmd_selection_fileAttributes->path," ") != NULL) g_string_replace(readlineResultString, "%s", "'%s'", 1);
 		  g_string_replace(readlineResultString, "%s",stdin_cmd_selection_fileAttributes->path, 1);