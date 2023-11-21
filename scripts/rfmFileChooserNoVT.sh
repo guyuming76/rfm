19081ce321c5f22bfcfefb4f4eb25b173e86c974
guyuming76 <gu_yu_ming@hotmail.com>
2023-11-21 12:55:38 +0800

    rfmFileChooserNoVT4searchResult.sh /home/guyuming/rfm/rfm.c < <(locate rfm.c)

--- scripts/rfmFileChooserNoVT.sh
+++ scripts/rfmFileChooserNoVT.sh
@@ -5,3 +5,7 @@ if [ $# -eq 0 ]; then
 else
 	rfm -r -d "$@" 0>/dev/null 1>/dev/null
 fi
+
+## TODO:
+## why did i add 0>/dev/null 1>/dev/null here?
+## i cannot recall now. does the output to stdout cause trouble to g_spawn in caller?
