f5c9bf92560a39f4c766165d1f3774c78301f0df
guyuming76 <gu_yu_ming@hotmail.com>
2023-12-11 13:57:52 +0800

    fix bug described in devPicAndVideo commit e740ca0dfd9ab729dbf3f48a77213cef5615f7cb. We now only use one thumbnail dir which hold normal size thumbnail. Two thumbnail size makes the program complicated. In future, we hope we can scale the iconview window as a whole. Thumbnail size can still be changed in config.h

--- config.def.h
+++ config.def.h
@@ -6,7 +6,6 @@
 #define RFM_TOOL_SIZE 22
 #define RFM_ICON_SIZE 48
 #define RFM_THUMBNAIL_SIZE 128 /* Maximum size for thumb dir normal is 128 */
-#define RFM_THUMBNAIL_LARGE_SIZE  RFM_THUMBNAIL_SIZE * 2
 #define RFM_MX_MSGBOX_CHARS 1500 /* Maximum chars for show_msg output; messages exceeding this will be displayed in parent stdout */
 #define RFM_MX_ARGS 128 /* Maximum allowed number of command line arguments in action commands below */
 #define RFM_MOUNT_MEDIA_PATH "/run/media" /* Where specified mount handler mounts filesystems (e.g. udisksctl mount) */
