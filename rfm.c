guyuming76/rfm

commit ab1db83e5a9a68420b2cd87e4ad179b2a0554169

don't generate thumbnail for thumbnail, show itself for picture in rfm_thumbDir

rfm.c
@@ -1055,8 +1055,13 @@ static RFM_ThumbQueueData *get_thumbData(GtkTreeIter *iter)
    thumbData->path=g_strdup(fileAttributes->path);
    thumbData->mtime_file = fileAttributes->file_mtime==NULL? 0 : g_date_time_to_unix(fileAttributes->file_mtime);
    thumbData->uri=g_filename_to_uri(thumbData->path, NULL, NULL);
-   thumbData->md5=g_compute_checksum_for_string(G_CHECKSUM_MD5, thumbData->uri, -1);
-   thumbData->thumb_name=g_strdup_printf("%s_%d.png", thumbData->md5, thumbData->thumb_size);
+   //don't generate thumbnail for thumbnail, show itself for picture in rfm_thumbDir
+   if (strncmp(rfm_thumbDir, thumbData->path, strlen(rfm_thumbDir))==0){
+     thumbData->md5=g_compute_checksum_for_string(G_CHECKSUM_MD5, thumbData->uri, -1);
+     thumbData->thumb_name=g_strdup_printf("%s_%d.png", thumbData->md5, thumbData->thumb_size);
+   }else{
+     thumbData->thumb_name=g_strdup(thumbData->path + strlen(rfm_thumbDir) + 1); //rfm_thumbDir does not have ending '/'
+   }
    thumbData->rfm_pid=getpid();  /* pid is used to generate a unique temporary thumbnail name */
 
    /* Map thumb path to model reference for inotify */

commit 6df62b31481cae70a0f422ab3725427e36d6b2da

fix bug introduced in commit ab1db83e5a9a68420b2cd87e4ad179b2a0554169

rfm.c
@@ -1056,11 +1056,11 @@ static RFM_ThumbQueueData *get_thumbData(GtkTreeIter *iter)
    thumbData->mtime_file = fileAttributes->file_mtime==NULL? 0 : g_date_time_to_unix(fileAttributes->file_mtime);
    thumbData->uri=g_filename_to_uri(thumbData->path, NULL, NULL);
    //don't generate thumbnail for thumbnail, show itself for picture in rfm_thumbDir
-   if (strncmp(rfm_thumbDir, thumbData->path, strlen(rfm_thumbDir))==0){
+   if (strncmp(rfm_thumbDir, thumbData->path, strlen(rfm_thumbDir))!=0){
      thumbData->md5=g_compute_checksum_for_string(G_CHECKSUM_MD5, thumbData->uri, -1);
      thumbData->thumb_name=g_strdup_printf("%s_%d.png", thumbData->md5, thumbData->thumb_size);
    }else{
-     thumbData->thumb_name=g_strdup(thumbData->path + strlen(rfm_thumbDir) + 1); //rfm_thumbDir does not have ending '/'
+     thumbData->thumb_name=g_strdup(thumbData->path + strlen(rfm_thumbDir) + 1); //get the thumbnail filename without rfm_thumbDir path,  rfm_thumbDir does not have ending '/', so +1
    }
    thumbData->rfm_pid=getpid();  /* pid is used to generate a unique temporary thumbnail name */
 