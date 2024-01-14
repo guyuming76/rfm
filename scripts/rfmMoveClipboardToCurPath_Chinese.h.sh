b99cc7d562a3d81cbc4441b2544f66203e4c3701
guyuming76 <gu_yu_ming@hotmail.com>
2024-01-14 11:08:53 +0800

    fix bug similar to commit  abf406cabdbb5ff7970149ef4c7bfdcdadb93971

--- scripts/rfmMoveClipboardToCurPath_Chinese.h.sh
+++ scripts/rfmMoveClipboardToCurPath_Chinese.h.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#set -x
 
-set -x
+sourcefiles=$(wl-paste)
+# TODO: wl-paste is for wayland, what if x11?
+target=$(pwd)
+# rfm使用g_spawn执行脚本时,第一个参数working directory会传入 rfm_curPath, 我的理解这里 pwd 就会得到 rfm_curPath 的值
 
-mv -i -v "$(wl-paste)" "$(pwd)"
-
-read -p "按回车关闭当前窗口"
+if [[ ! -z "$target" ]]; then
+	for sourcefile in $sourcefiles;do
+		mv -i -v $sourcefile $target
+	done
+	read -p "按回车关闭当前窗口"
+else
+	echo "pwd return empty"
+	exit 2
+fi
