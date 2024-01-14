abf406cabdbb5ff7970149ef4c7bfdcdadb93971
guyuming76 <gu_yu_ming@hotmail.com>
2024-01-14 10:37:27 +0800

    fix bug that paste multiple filenames in clipboard does not work. TODO: we may have a file contextual menu so that user can copy clipboard filename to selected filename, that is, overwrite the selected filename. currently, we only have this on toolbar

--- scripts/rfmCopyClipboardToCurPath_English.h.sh
+++ scripts/rfmCopyClipboardToCurPath_English.h.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#set -x
 
-set -x
+sourcefiles=$(wl-paste)
+# TODO: wl-paste is for wayland, what if x11?
+target=$(pwd)
+# rfm使用g_spawn执行脚本时,第一个参数working directory会传入 rfm_curPath, 我的理解这里 pwd 就会得到 rfm_curPath 的值
 
-cp -i -v "$(wl-paste)" "$(pwd)"
-
-read -p "press enter to close current window"
+if [[ ! -z "$target" ]]; then
+	for sourcefile in $sourcefiles;do
+		cp -i -v -r $sourcefile $target
+	done
+	read -p "press enter to close current window"
+else
+	echo "pwd return empty"
+	exit 2
+fi
