e214ba169ce114ebcdf5b5eb8b2cdf59142c33c7
guyuming76 <gu_yu_ming@hotmail.com>
2024-01-15 18:54:31 +0800

    add TODO

--- scripts/rfmCopyClipboardToCurPath_Chinese.h.sh
+++ scripts/rfmCopyClipboardToCurPath_Chinese.h.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #set -x
 
+#read -p "从剪贴板读入源文件名还是打开新rfm窗口选取源文件,默认从剪贴板读,若选择新rfm窗口,则输入源文件所在目录" sourceDirectory
+#TODO: 完称上面提示后续代码
+
 sourcefiles=$(wl-paste)
 # TODO: wl-paste is for wayland, what if x11?
 target=$(pwd)
