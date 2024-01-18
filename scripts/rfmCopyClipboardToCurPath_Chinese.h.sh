684f58b08aedfeb448ff3efd06386d34c7b55a48
guyuming76 <gu_yu_ming@hotmail.com>
2024-01-18 14:52:42 +0800

    This does not work as expected. it has problem as described in submodule commit id 82ce6bc2b92862891bb9057d3ae477d7a2f4d660, how can i open a namedpipe in bash?

--- scripts/rfmCopyClipboardToCurPath_Chinese.h.sh
+++ scripts/rfmCopyClipboardToCurPath_Chinese.h.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 #set -x
 
-#read -p "从剪贴板读入源文件名还是打开新rfm窗口选取源文件,默认从剪贴板读,若选择新rfm窗口,则输入源文件所在目录" sourceDirectory
-#TODO: 完称上面提示后续代码
-
-sourcefiles=$(wl-paste)
-# TODO: wl-paste is for wayland, what if x11?
 target=$(pwd)
 # rfm使用g_spawn执行脚本时,第一个参数working directory会传入 rfm_curPath, 我的理解这里 pwd 就会得到 rfm_curPath 的值
 
-if [[ ! -z "$target" ]]; then
-	for sourcefile in $sourcefiles;do
-		cp -i -v -r $sourcefile $target
-	done
-	read -p "按回车关闭当前窗口"
+read -p "从剪贴板读入源文件名还是打开新rfm窗口选取源文件,默认从剪贴板读,若选择新rfm窗口,则输入源文件所在目录" sourceDirectory
+#TODO: 完称上面提示后续代码
+
+if [[ ! -z "$sourceDirectory" ]];then
+	if [[ -e $sourceDirectory ]];then
+		namedpipe="/tmp/rfmFileChooser_$$"
+		if [[ ! -p $namedpipe ]];then
+			mkfifo $namedpipe
+			#trap "rm -f $namedpipe" EXIT
+			while read line;do cp -i -v -r $line $target; done < $namedpipe &
+			rfm -d $sourceDirectory -r $namedpipe
+			#rm -f $namedpipe
+		else
+			echo "$namedpipe 已存在" > 2
+			exit 4
+		fi
+	else
+		echo "输入目录或文件不存在" > 2
+		exit 3
+	fi
 else
-	echo "pwd return empty"
-	exit 2
+
+	sourcefiles=$(wl-paste)
+	# TODO: wl-paste is for wayland, what if x11?
+
+	if [[ ! -z "$target" ]]; then
+		for sourcefile in $sourcefiles;do
+			cp -i -v -r $sourcefile $target
+		done
+	else
+		echo "pwd return empty" > 2
+		exit 2
+	fi
 fi
