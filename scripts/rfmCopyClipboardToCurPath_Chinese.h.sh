504faefdcaff4b986dfbc2946015919f08a71309
guyuming76 <gu_yu_ming@hotmail.com>
2024-01-24 09:54:52 +0800

    use history in bash read

--- scripts/rfmCopyClipboardToCurPath_Chinese.h.sh
+++ scripts/rfmCopyClipboardToCurPath_Chinese.h.sh
@@ -4,10 +4,15 @@
 target=$(pwd)
 # rfm使用g_spawn执行脚本时,第一个参数working directory会传入 rfm_curPath, 我的理解这里 pwd 就会得到 rfm_curPath 的值
 
+historyFile=~/.rfm_history_directory
+history -r
+
 echo "从剪贴板读入源文件名还是打开新rfm窗口选取源文件?"
 echo "直接回车默认从剪贴板读;"
 echo "输入源文件所在目录,则打开新rfm窗口选择源文件:"
-read sourceDirectory
+read -e sourceDirectory
+history -s "$sourceDirectory"
+history -w
 
 if [[ ! -z "$sourceDirectory" ]];then
 	if [[ -e $sourceDirectory ]];then
