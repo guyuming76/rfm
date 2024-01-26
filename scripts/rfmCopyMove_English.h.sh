2d2b42b0921a396fc4b8440fec783c7e362a9d42
guyuming76 <gu_yu_ming@hotmail.com>
2024-01-26 15:59:49 +0800

    make change to english file according

--- scripts/rfmCopyMove_English.h.sh
+++ scripts/rfmCopyMove_English.h.sh
@@ -3,25 +3,24 @@
 
 #set -x
 
-historyFile=~/.rfm_history_directory
-history -r
-history -s "$(pwd)"
-# 用户很有可能是在查询结果视图里面选中文件,然后复制到当前目录的,所以目的地默认为当前目录还是很有必要,虽然会遇到来源文件也是当前目录的情况, 我测试的结果是cp -i 会直接忽略这个文件,并不提示
-
 echo "press enter to copy selected filenames into clipboard;"
 echo "or input the destination(fullpath or relative to current $(pwd)), up and down arrow keys can be used to choose from input history:"
 
-read -e -r input_destination
+input_destination="$(bash -c -i 'HISTFILE=~/.rfm_history_directory;
+				HISTCONTROL=ignoreboth;
+				history -r;
+				history -s "$(pwd)";
+				read -e -r input;
+				history -s "$input";
+				history -w;
+				echo $input')"
 
 
 if [[ -z "$input_destination" ]]; then
 	echo "${@:2}" | wl-copy
 	# TODO: wl-copy is for wayland, what if x11?
 else
-	history -s "$input_destination"
 	destination="$(realpath -s $input_destination)"
-	history -s "$destination"
-	history -w
 
 	if [[ -e $destination ]]; then
 		if [[ -d $destination ]];then
