7522f07f7d267dca20bc5dd0f3ef174093cc5919
guyuming76 <gu_yu_ming@139.com>
2024-05-18 09:24:48 +0000

    remove useless code and add TODO

--- config.def.h
+++ config.def.h
@@ -76,6 +76,9 @@ static const char *git_stage_cmd[] = {"/usr/bin/git","stage",NULL};
 static const char *git_root_cmd[] = {"/usr/bin/git","rev-parse", "--show-toplevel",NULL};
 static const char *git_commit_message_cmd[] = {"/usr/bin/git","log","-1","--oneline",NULL};
 static const char *git_log_cmd[] = { rfmBinPath "/rfmVTforCMD_hold.sh","/usr/bin/git","log",NULL};
+//现在只有很少的像上面这行的情况需要rfmVTforCMD_hold.sh脚本，如果想去掉它，统一用rfmVTforCMD.sh,就得想办法把类似read -p 这种代码插到上面的数组里，如果直接把 read 命令放在git log后面，NULL的前面，目前就会牺牲可以在末尾追加任意个选中文件的功能，我记得加一个“”数组项可以表示替换一个文件
+//若利用stdin_command_bash 这样的返回数组的函数，就意味着要增强文件上下文菜单的配置功能，让其能够接受函数，但这样会增加代码的复杂性，我认为上下文菜单应该保持只接受静态数组这么一种相对简单的配置方式
+//TODO：所以，现在要么保持有些冗余的rfmVTforCMD_hold.sh脚本，要么增强把静态数组和当前选中文件列表合并的方式，增加一个在中间合并进全部文件的符号，这个可以和命令行中间使用%s符号替换文件名一起考虑
 static const char *tig_cmd[] = { rfmBinPath "/rfmVTforCMD.sh","/usr/bin/tig",NULL};
 static const char *git_show_pics_cmd[] = { rfmBinPath "/rfmVTforCMD.sh", rfmBinPath "/rfmGitShowPictures.sh",NULL};
 static const char *git_commit[] = { rfmBinPath "/rfmVTforCMD.sh", rfmBinPath "/rfmGitCommit.sh",NULL};
@@ -286,9 +289,7 @@ static gboolean ignored_filename(gchar *name){
 static gchar shell_cmd_buffer[ARG_MAX]; //TODO: notice that this is shared single instance. But we only run shell command in sync mode now. so, no more than one thread will use this
 static gchar* stdin_cmd_template_bash[]={"/bin/bash","-i","-c", shell_cmd_buffer, NULL};
 static gchar* stdin_cmd_template_nu[]={"nu","-c", shell_cmd_buffer, NULL};
-static gchar* stdin_cmd_template_bash_inNewVT[]  = { rfmBinPath "/rfmVTforCMD_hold.sh", "/bin/bash","-i","-c",shell_cmd_buffer, NULL };
 static gchar* stdin_cmd_template_bash_newVT_nonHold[] = { rfmBinPath "/rfmVTforCMD.sh", "/bin/bash", "-i", "-c", shell_cmd_buffer, NULL };
-static gchar* stdin_cmd_template_nu_inNewVT[] = { rfmBinPath "/rfmVTforCMD_hold.sh", "nu", "-c", shell_cmd_buffer, NULL };
 static gchar* stdin_cmd_template_nu_inNewVT_nonHold[] = { rfmBinPath "/rfmVTforCMD.sh", "nu", "-c", shell_cmd_buffer, NULL };
 static gchar** stdin_command_bash(gchar* user_input_cmd, gboolean inNewVT) {
   if (inNewVT){
@@ -303,6 +304,7 @@ static gchar** stdin_command_bash(gchar* user_input_cmd, gboolean inNewVT) {
   }
 }
 static gchar** stdin_command_nu(gchar* user_input_cmd, gboolean inNewVT) {
+//TODO: add read -p into shell_cmd_buffer when inNewVT like stdin_command_bash
   sprintf(shell_cmd_buffer,"%s",g_strdup(user_input_cmd));
   return inNewVT? stdin_cmd_template_nu_inNewVT_nonHold : stdin_cmd_template_nu;
 }
