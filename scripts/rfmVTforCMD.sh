0429294b0fdf26d2f6c3a7a5823747b6ee5df8f9
guyuming76 <gu_yu_ming@139.com>
2024-05-18 07:41:29 +0000

    use RFM_TERM environmental variable to specify terminal emulator, you can set it in something like .bashrc, you may also set it with different value depending on whether it is in X or wayland This change goes together with next change in config.def.h, i put them in different commits simply because i can cherry-pick this commit easyly to the 20240508split_code branch, while i would redo the change for config.def.h manaully in VT.h in that branch.

--- scripts/rfmVTforCMD.sh
+++ scripts/rfmVTforCMD.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 #LIBGL_ALWAYS_SOFTWARE=1 /usr/bin/alacritty -e "$@"
+#foot -t xterm-256color --log-level=warning "$@"
 
-foot -t xterm-256color --log-level=warning "$@"
+if [[ -n "$RFM_TERM" ]];then
+	cmd="$RFM_TERM "$@""
+	eval $cmd
+else
+	echo "environmental variable RFM_TERM is empty, please set it with terminial emulator name and parameters" > 2
+	exit 1
+fi
