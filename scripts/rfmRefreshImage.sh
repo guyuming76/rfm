6a4f9127cb07c84dccbb50b3ef3016bd8322c322
guyuming76 <gu_yu_ming@hotmail.com>
2024-05-19 09:22:10 +0800

    open image with xdg-open if imv is not installed

--- scripts/rfmRefreshImage.sh
+++ scripts/rfmRefreshImage.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ ! -e "$(which imv-msg)" ]];then
+	xdg-open "$@"
+	exit
+fi
+
 # imv 有时启动子进程 imv-wayland 来显示图片，imv-msg需要和这个子进程号通信，所以我下面用了 tail -n1 来选取最后一个满足条件的进程
 # 当config.def.h 里面选RFM_EXEC_NONE时,${PPID}会是1;选RFM_EXEC_STDOUT时,${PPID}会是rfm的进程PID
 
@@ -12,3 +17,4 @@ else
     	imv-msg $imv_pid close all
 	imv-msg $imv_pid open "$@"
 fi
+
