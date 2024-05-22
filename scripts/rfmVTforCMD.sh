bd06be65dd4202194c4181ff8c137b6649cf0bbd
guyuming76 <gu_yu_ming@hotmail.com>
2024-05-22 20:24:06 +0800

    echo debug info into /tmp/test_rfmVTforCMD.sh.txt

--- scripts/rfmVTforCMD.sh
+++ scripts/rfmVTforCMD.sh
@@ -3,8 +3,14 @@
 #LIBGL_ALWAYS_SOFTWARE=1 /usr/bin/alacritty -e "$@"
 #foot -t xterm-256color --log-level=warning "$@"
 
+echo "$@" > /tmp/test_rfmVTforCMD.sh.txt
+for arg in "$@"; do
+        echo $arg >> /tmp/test_rfmVTforCMD.sh.txt
+done
+
 if [[ -n "$RFM_TERM" ]];then
 	cmd="$RFM_TERM "$@""
+	echo $cmd >> /tmp/test_rfmVTforCMD.sh.txt
 	eval $cmd
 else
 	echo "environmental variable RFM_TERM is empty, please set it with terminial emulator name and parameters" >&2
