dabe6601d77b1671fac6fea32abb1429ac3920d6
guyuming76 <gu_yu_ming@hotmail.com>
2024-08-23 14:02:33 +0800

    my test on the mineral repository shows that loading thumbnail pictures is fast. So do not load thumbnail in idle one by one.

--- rfm.c
+++ rfm.c
@@ -1896,13 +1896,14 @@ static void refresh_store(RFM_ctx *rfmCtx)
        g_dir_close(dir);
 
        Iterate_through_fileAttribute_list_to_insert_into_store();
-       if (rfm_do_thumbs == 1 && g_file_test(rfm_thumbDir, G_FILE_TEST_IS_DIR) && gtk_tree_model_get_iter_first(treemodel, &thumbnail_load_iter))
-	   rfm_thumbLoadScheduler=g_idle_add((GSourceFunc)iterate_through_store_to_load_thumbnails_or_enqueue_thumbQueue_one_by_one_when_idle, &thumbnail_load_iter);
+
 #ifdef GitIntegration
        if (get_treeviewColumnByEnum(COL_GIT_COMMIT_MSG)->Show && gtk_tree_model_get_iter_first(treemodel, &gitMsg_load_iter)) rfm_gitCommitMsgScheduler=g_idle_add((GSourceFunc)iterate_through_store_to_load_gitCommitMsg_one_by_one_when_idle, &gitMsg_load_iter);
 #endif
-       //In_refresh_store = FALSE;
-       //gtk_widget_set_sensitive(PathAndRepositoryNameDisplay, TRUE);
+
+       if (rfm_do_thumbs == 1 && g_file_test(rfm_thumbDir, G_FILE_TEST_IS_DIR) && gtk_tree_model_get_iter_first(treemodel, &thumbnail_load_iter))
+	 //rfm_thumbLoadScheduler=g_idle_add((GSourceFunc)iterate_through_store_to_load_thumbnails_or_enqueue_thumbQueue_one_by_one_when_idle, &thumbnail_load_iter);
+	 while(iterate_through_store_to_load_thumbnails_or_enqueue_thumbQueue_one_by_one_when_idle(&thumbnail_load_iter)){};
      }
   }
 #ifdef GitIntegration
