guyuming76/rfm

commit bfa43ce5c72e6b56c2edc164a2657946f92425a6

This is better then the branch KeepCurPathInWindowTitle

rfm.c
@@ -1655,9 +1655,11 @@ static gint sort_func(GtkTreeModel *model, GtkTreeIter *a, GtkTreeIter *b, gpoin
 }
 
 static void set_Titles(gchar * title){
-   gtk_window_set_title(GTK_WINDOW(window), title);
-   gtk_tool_button_set_label(PathAndRepositoryNameDisplay, title);
-   if (startWithVT() && !(StartedAs_rfmFileChooser && rfmFileChooserReturnSelectionIntoFilename==NULL)) set_terminal_window_title(title);
+   if (title==NULL) return;
+   gboolean title_is_rfm_curPath = (title[0]=='/');
+   if (title_is_rfm_curPath) gtk_window_set_title(GTK_WINDOW(window), title);
+   if (!(SearchResultViewInsteadOfDirectoryView && title_is_rfm_curPath)) gtk_tool_button_set_label(PathAndRepositoryNameDisplay, title);
+   if (title_is_rfm_curPath && startWithVT() && !(StartedAs_rfmFileChooser && rfmFileChooserReturnSelectionIntoFilename==NULL)) set_terminal_window_title(title);
    g_free(title);
 }
 
@@ -1819,10 +1821,16 @@ static void set_rfm_curPath(gchar* path)
    /* if (e=read_history(g_build_filename(rfm_curPath,".rfm_history", NULL))) */
    /*     g_warning("failed to read_history(%s) error code:%d. it's normal if you enter this directory for the first time with rfm.",g_build_filename(rfm_curPath,".rfm_history", NULL),e); */
    /* history_entry_added=0; */
+
 #ifdef GitIntegration
    g_spawn_wrapper(git_inside_work_tree_cmd, NULL, RFM_EXEC_OUPUT_READ_BY_PROGRAM, NULL, FALSE, set_curPath_is_git_repo, NULL);
-   /* if (!rfmReadFileNamesFromPipeStdIn && curPath_is_git_repo) */
-   /*    g_spawn_wrapper(git_current_branch_cmd, NULL, 0, RFM_EXEC_OUPUT_READ_BY_PROGRAM, NULL, TRUE, set_window_title_with_git_branch, NULL); */
+   if (SearchResultViewInsteadOfDirectoryView){
+     if (curPath_is_git_repo)
+       g_spawn_wrapper(git_current_branch_cmd, NULL, RFM_EXEC_OUPUT_READ_BY_PROGRAM, NULL, TRUE, set_window_title_with_git_branch_and_sort_view_with_git_status, NULL);
+     else set_Titles(g_strdup(rfm_curPath));
+   }
+#else
+   if (SearchResultViewInsteadOfDirectoryView) set_Titles(g_strdup(rfm_curPath));
 #endif
 }
 

commit cc2281fb92d93ee8e4cd1e17fd26076d6a39aac4

this may fix a bug caused in commit  bfa43ce5c72e6b56c2edc164a2657946f92425a6. But i have not noticed anything wrong by this code during use, just notice this when reviewing code. Maybe this is because similiar code is executed in refresh_store and override this bug. So, we may remove these title setting lines from set_rfm_curPath and leave them only in refresh_store.

rfm.c
@@ -2078,13 +2078,13 @@ static void set_rfm_curPath(gchar* path)
 
 #ifdef GitIntegration
      g_spawn_wrapper(git_inside_work_tree_cmd, NULL, G_SPAWN_DEFAULT, NULL, FALSE, set_curPath_is_git_repo, NULL,TRUE);
-   if (SearchResultViewInsteadOfDirectoryView){
+   if (SearchResultViewInsteadOfDirectoryView^1){
      if (curPath_is_git_repo)
        g_spawn_wrapper(git_current_branch_cmd, NULL, G_SPAWN_DEFAULT, NULL, TRUE, set_window_title_with_git_branch_and_sort_view_with_git_status, NULL,TRUE);
      else set_Titles(g_strdup(rfm_curPath));
    }
 #else
-   if (SearchResultViewInsteadOfDirectoryView) set_Titles(g_strdup(rfm_curPath));
+   if (SearchResultViewInsteadOfDirectoryView^1) set_Titles(g_strdup(rfm_curPath));
 #endif
 }
 