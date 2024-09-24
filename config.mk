200a4b992b8c02b8510821c37ed997699d8aadca
guyuming76 <gu_yu_ming@hotmail.com>
2024-09-25 03:47:58 +0800

    move rfmBinPath configuration into config.mk

--- config.mk
+++ config.mk
@@ -1,6 +1,10 @@
 PREFIX = /usr/local
 # if installed with gentoo ebuild, the ebuild script will remove /local so that files will be installed into /usr/bin instead of /usr/local/bin
 
+rfmBinPath =\"${PREFIX}/bin/\"
+#rfmBinPath = \"\"
+# you can set rfmBinPath to \"\" if you want to use executable or scripts in search path
+
 languageInclude = Chinese.h
 #languageInclude = English.h
 
